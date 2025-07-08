d7c1942d6c69070ddf6358bcf2df0b38b2ac909f
seifessam60 <seifessam60@gmail>
2025-07-08 12:12:25 +0300

    Creating Posts

---
+++ src/actions/post.action.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+import { getDbUserId } from "./user.action";
+import { revalidatePath } from "next/cache";
+
+export async function createPost(content: string, imageUrl: string) {
+  try {
+    const userId = await getDbUserId();
+    const post = prisma.post.create({
+      data: {
+        content,
+        image: imageUrl,
+        authorId: userId,
+      },
+    });
+    revalidatePath("/");
+    return { success: true, post };
+  } catch (error) {
+    console.log("Error creating post:", error);
+    return { success: false, error: "Failed to create post" };
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import CreatePost from "@/components/CreatePost";
+import { currentUser } from "@clerk/nextjs/server";
 
-export default  function Home() {
-
+export default async function Home() {
+  const user = await currentUser();
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">{user && <CreatePost />}</div>
+      <div className="hidden lg:block lg:col-span-4 sticky top-20"></div>
     </div>
   );
 }
