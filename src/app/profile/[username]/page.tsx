e88ffd3775c2eabd84c716b479a1f0bfbec69d5d
seifessam60 <seifessam60@gmail>
2025-07-15 20:10:22 +0300

    pug fixed

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -7,12 +7,11 @@ import {
 import { notFound } from "next/navigation";
 import ProfilePageClient from "./ProfilePageClient";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { username: string };
+export async function generateMetadata(props: {
+  params: Promise<{ username: string }>;
 }) {
-  const user = await getProfileByUsername(params.username);
+  const { username } = await props.params;
+  const user = await getProfileByUsername(username);
   if (!user) return;
 
   return {
@@ -21,8 +20,11 @@ export async function generateMetadata({
   };
 }
 
-async function ProfilePageServer({ params }: { params: { username: string } }) {
-  const user = await getProfileByUsername(params.username);
+async function ProfilePageServer(props: {
+  params: Promise<{ username: string }>;
+}) {
+  const { username } = await props.params;
+  const user = await getProfileByUsername(username);
 
   if (!user) notFound();
 
